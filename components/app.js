9d7dca70391518a1eea5654e0842f72790902c54
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-07-04 19:22:09 +0200

    add JS changes

--- components/app.js
+++ components/app.js
@@ -54,22 +54,22 @@ var Counter = React.createClass({
 					React.createElement('h2', {}, 'Counter'),
 					React.createElement('span', {}, 'Counter: ' + this.state.counter),
 						React.createElement('div', {}, 
-							React.createElement('button', {
-								className: 'incr_button', 
-								onClick: this.increment},'Increment Counter'),
+						React.createElement('button', {
+							className: 'incr_button', 
+							onClick: this.increment},'Increment Counter'),
 							
-							React.createElement('button', {
-								onClick: this.decrement}, 'Decrement Counter')
+						React.createElement('button', {
+							onClick: this.decrement}, 'Decrement Counter')
 					)
 				)
 		);
 	}
 });
 
-var element = React.createElement('div', {className: 'counters'},
-		React.createElement('h1', {}, 'Template of counters'),
-		React.createElement(Counter),
-		React.createElement(Counter)
-	);
+	var element = React.createElement('div', {className: 'counters'},
+			React.createElement('h1', {}, 'Template of counters'),
+			React.createElement(Counter),
+			React.createElement(Counter)
+		);
 
 ReactDOM.render(element, document.getElementById('app'));
