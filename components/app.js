078e2dd9d1fe19ad00731955917d99197d21f06f
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-07-04 21:41:27 +0200

    add JS changes

--- components/app.js
+++ components/app.js
@@ -3,8 +3,7 @@ var Counter = React.createClass({
 	getInitialState: function(){
 		return {
 			counter: 0
-		};
-		
+		};	
 	},
 	
 	increment: function(){
@@ -50,26 +49,26 @@ var Counter = React.createClass({
 	
 	render: function(){
 		return (
-				React.createElement('div', {className: 'counter'},
-					React.createElement('h2', {}, 'Counter'),
-					React.createElement('span', {}, 'Counter: ' + this.state.counter),
-						React.createElement('div', {}, 
+			React.createElement('div', {className: 'counter'},
+				React.createElement('h2', {}, 'Counter'),
+				React.createElement('span', {}, 'Counter: ' + this.state.counter),
+					React.createElement('div', {}, 
 						React.createElement('button', {
 							className: 'incr_button', 
 							onClick: this.increment},'Increment Counter'),
 							
 						React.createElement('button', {
 							onClick: this.decrement}, 'Decrement Counter')
-					)
 				)
+			)
 		);
 	}
 });
 
-	var element = React.createElement('div', {className: 'counters'},
-			React.createElement('h1', {}, 'Template of counters'),
-			React.createElement(Counter),
-			React.createElement(Counter)
+var element = React.createElement('div', {className: 'counters'},
+		React.createElement('h1', {}, 'Template of counters'),
+		React.createElement(Counter),
+		React.createElement(Counter)
 		);
 
 ReactDOM.render(element, document.getElementById('app'));
